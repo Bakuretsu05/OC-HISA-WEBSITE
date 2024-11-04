db43e3c30d58c6122421568c708bfdd76fd35bb7
Bakuretsu <50520529+Bakuretsu05@users.noreply.github.com>
2024-11-04 18:19:55 +0800

    Add sponsors into Partner page

--- src/pages/partner.jsx
+++ src/pages/partner.jsx
@@ -1,16 +1,35 @@
-import SponsorImg from "../assets/images/sponsorship.jpg";
+import VerticalCard from "../components/VerticalCard.jsx";
+
+import sponsorsData from "../Datas/sponsors.json";
 
 const Partner = () => {
   return (
-    <div className="bg-slate-700 h-screen p-16 flex flex-col items-center">
-      <h1 className="text-6xl font-bold text-white">Our Sponsor</h1>
-      <div className="flex items-center h-full gap-10">
-        <img
-          src={SponsorImg}
-          alt="sponsor"
-          className="h-3/4 rounded-3xl shadow-2xl hover:h-5/6 duration-500"
-        />
-        <p className="text-white text-3xl">"Today's UberOne is on us!"</p>
+    // <div className="bg-slate-700 h-screen p-16 flex flex-col items-center">
+    //   <h1 className="text-6xl font-bold text-white">Our Sponsor</h1>
+    //   {sponsorsData.map((sponsor) => (
+    //     <VerticalCard
+    //       key={sponsor.id}
+    //       title={sponsor.name}
+    //       text={sponsor.description || null}
+    //       img={sponsor.logoUrl}
+    //     />
+    //   ))}
+    // </div>
+    <div className="py-8 px-56 h-screen">
+      <h2 className="text-2xl font-semibold text-center mb-6">Our Sponsors</h2>
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+        {sponsorsData.map((sponsor) => (
+          <div
+            key={sponsor.id}
+            className="bg-white shadow-lg rounded-lg p-4 flex justify-center items-center"
+          >
+            <img
+              src={sponsor.logoUrl}
+              alt={sponsor.name}
+              className="w-full h-auto object-contain"
+            />
+          </div>
+        ))}
       </div>
     </div>
   );
