ea2226fb48c96bc7853d321606337cf0ab4e849f
Bakuretsu <50520529+Bakuretsu05@users.noreply.github.com>
2025-01-13 19:07:48 +0800

    Add mobile design for homepage

--- src/components/HomePage/StudentEssentialsSection.jsx
+++ src/components/HomePage/StudentEssentialsSection.jsx
@@ -2,7 +2,7 @@ import ganbuPicture from "../../assets/images/student-essentials-pic.png";
 
 const StudentEssentialsSection = () => {
   return (
-    <div className="relative w-full h-screen bg-gray-900">
+    <div className="relative w-full h-96 sm:h-screen bg-gray-900">
       {/* Left-side image with blue overlay */}
       <div className="relative w-full h-full">
         <img
@@ -15,13 +15,15 @@ const StudentEssentialsSection = () => {
 
       {/* Right-side text content */}
       <div
-        className="absolute inset-y-0 p-10 right-0 w-4/5 bg-gray-800 text-white flex items-center"
+        className="absolute inset-y-0 pl-16 sm:p-10 right-0 w-4/5 bg-gray-800 text-white flex items-center"
         style={{
           clipPath: "polygon(100% 0, 100% 100%, 40% 100%, 0 0)",
         }}
       >
         <div className="p-8 ml-auto text-end">
-          <h1 className="text-5xl font-bold mb-4">Student Essentials</h1>
+          <h1 className="text-3xl sm:text-5xl font-bold mb-4">
+            Student Essentials
+          </h1>
           <p className="text-xl mb-6">
             We compiled essential information for new students coming to Taiwan
             into our{" "}
