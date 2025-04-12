b21d42db25aef3317cb237d44bc0871f427ac9bc
jeffreyhans <mjeffhans@gmail.com>
2025-04-12 23:29:12 +0800

    Fixed homepage overflow issue, Changed bg color for StudentEssentialSection

--- src/components/HomePage/AboutSection.jsx
+++ src/components/HomePage/AboutSection.jsx
@@ -3,20 +3,20 @@ import ganbuPicture from "../../assets/images/photo-1.png";
 
 const AboutSection = () => {
   return (
-    <div className="relative w-full h-screen bg-white sm:bg-gray-100 p-5 sm:p-0">
+    <div className="relative h-screen bg-white sm:bg-gray-100 p-5 sm:p-0">
       <img
-        className="hidden sm:block absolute inset-0 h-full left-[40%]"
+        className="hidden sm:block absolute object-fill h-full right-0"
         src={ganbuPicture}
       />
 
-      <div className="hidden sm:block absolute inset-0 right-0 h-full w-full bg-orange-500 opacity-35"></div>
+      <div className="hidden sm:block absolute inset-0 right-0 h-full bg-orange-500 opacity-35"></div>
 
-      <div className="relative z-10 w-full sm:w-2/3 h-full bg-white rounded-r-full sm:shadow-card-shadow sm:p-16 flex items-center">
+      <div className="relative z-10 sm:w-2/3 h-full bg-white rounded-r-full sm:shadow-card-shadow sm:p-16 flex items-center">
         <div className="sm:w-4/5 text-center sm:text-left">
-          <h1 className="text-6xl sm:text-8xl font-bold mb-6 text-black">
+          <h1 className="md:text-6xl text-8xl font-bold mb-6 text-black">
             OC-HISA
           </h1>
-          <p className="text-2xl text-gray-700 mb-4">
+          <p className="text-xl text-gray-700 mb-4">
             We are a community of Indonesian students in Hsinchu, Taiwan. Our
             members are Indonesian students from two Taiwan national
             universities in Hsinchu: National Tsing Hua University (NTHU) and
--- src/components/HomePage/StudentEssentialsSection.jsx
+++ src/components/HomePage/StudentEssentialsSection.jsx
@@ -8,14 +8,14 @@ const StudentEssentialsSection = () => {
         <img
           src={ganbuPicture}
           alt="Students"
-          className="absolute h-full scale-110 z-0"
+          className="absolute h-full scale-100 -z-0"
         />
         <div className="absolute inset-0 bg-white opacity-30"></div>
       </div>
 
       {/* Right-side text content */}
       <div
-        className="absolute inset-y-0 pl-16 sm:p-10 right-0 w-4/5 bg-gray-800 text-white flex items-center"
+        className="absolute inset-y-0 pl-16 sm:p-10 right-0 w-4/5 bg-signature-red text-white flex items-center"
         style={{
           clipPath: "polygon(100% 0, 100% 100%, 40% 100%, 0 0)",
         }}
@@ -24,10 +24,10 @@ const StudentEssentialsSection = () => {
           <h1 className="text-3xl sm:text-5xl font-bold mb-4">
             Student Essentials
           </h1>
-          <p className="text-xl mb-6">
+          <p className="text-xl mb-6 w-[40rem]">
             We compiled essential information for new students coming to Taiwan
             into our{" "}
-            <a href="#" className="text-blue-300 underline">
+            <a href="#" className="font-bold hover:underline">
               OC-HISA guidebook
             </a>
             .
